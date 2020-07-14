9d6fac438f172e00158f7b473d49b4ce39f7889e
Daniel Machado <daniel.machado.av@gmail.com>
2020-07-14 01:11:32 -0300

    Programa meio tonto que executa um for

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VamosNoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n   Ohhhh");
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < 15; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine("      What has just happened now?");
+                }
+                else
+                {
+                    Console.WriteLine("   here we go");
+                }
+
+                Console.WriteLine("\n");
+
+            }
+        }
+    }
+}
